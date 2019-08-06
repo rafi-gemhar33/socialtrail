23adb0bb4a733f74fa45700d9ea0a616728a05b1
sajan <sajanloveu123@gmail.com>
2019-08-06 17:27:05 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# SOCIAL MEDIA TRACKER
+1 keep track of your followers
+2 keep track of your likes/twites
+3 keep track of comments
---
+++ client/components/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+const HomePage = () => {
+  return (
+    <div>
+    	<h1>Social Media Tracker</h1>
+    </div>
+  );
+}
+
+export default HomePage;
