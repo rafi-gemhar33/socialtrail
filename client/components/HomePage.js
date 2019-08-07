68c14388915bf66607cb71344398fc8b4346e1a0
Gemhar33 <gemharedu@gmail.com>
2019-08-07 16:40:32 +0530

    adding twitter package and endpoint for searching users

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Login from './Login';
+import SearchUser from "./SearchUser";
 
 const HomePage = () => {
   return (
     <div>
     	<h1>Social Media Tracker</h1>
-    	<login />
+    	<Login />
+      <SearchUser />
     </div>
   );
 }
 
 export default HomePage;
+
