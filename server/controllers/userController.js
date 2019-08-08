30e9a387ce2232860da5f337b15dcd514b30c64b
sajan <sajanloveu123@gmail.com>
2019-08-08 10:55:26 +0530

    add user login sighnup in backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,12 +12,37 @@ module.exports = {
 	},
 
 	login: (req,res) => {
-		console.log('rendered');
+		console.log(req.body,'rendered');
+		User.findOne({ email: req.body.email }, (err,user) => {
+			if(err){
+				return res.status(500).json({ error: err, success: false, massege: "Server error" });
+			} else if(!user){ 
+				return res.status(400).json({ success: false, massege: "User does'nt exist" });
+			} else if(user){
+				const token = jwtAuth.signToken(user._id);
+				return res.status(200).json({ success: true, user, token });
+			}
+		})
 	
 	},
 
 	signUp: (req,res) => {
-		console.log('rendered');
+		console.log(req.body,'rendered');
+		User.findOne({ email: req.body.email }, (err, user) => {
+			if(err){
+				return res.status(500).json({ error: err, success: false, massege: "Server error" });
+			} else if(user){ 
+				return res.status(403).json({ success: false, massege: "User alredy exist" });
+			} else if(!user){ 
+				User.create(req.body, (err, user) => {
+					if(err){ 
+						return res.status(500).json({ success: false, massege: "Server error" });
+					}	else if(user){ 
+						return res.status(200).json({ success: true, massege: "User registred sucessfully" });
+					}
+				})
+			}
+		})
 	
 	},
 
