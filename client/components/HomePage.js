81030b1d4e694b363f0892fb6752b077d0571180
sajan <sajanloveu123@gmail.com>
2019-08-10 10:58:39 +0530

    add pretier

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-import SearchUser from "./SearchUser";
-import Canvas from "./Canvas";
+import SearchUser from './SearchUser';
+import Canvas from './Canvas';
 
 const HomePage = () => {
 	return (
