3062410e398c24bf57d437d82eb9cdc40b7c655f
sajan <sajanloveu123@gmail.com>
2019-08-13 15:58:05 +0530

    add active class in nav.js navlinks

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 class Navbar extends Component {
@@ -20,28 +20,36 @@ class Navbar extends Component {
 						</Link>
 						<ul id="nav-mobile" className="right hide-on-med-and-down">
 							{isAuthInProgress ? (
-								<>
+								<div>
 									<li>
-										<Link to="/">Home</Link>
+										<NavLink to="/" exact activeClassName="active" isActive={this.checkActive}>
+											Home
+										</NavLink>
 									</li>
 									<li>
-										<Link to="/login">Login</Link>
+										<NavLink to="/login" exact activeClassName="active" isActive={this.checkActive}>
+											Login
+										</NavLink>
 									</li>
 									<li>
-										<Link to="/register">SignUp</Link>
+										<NavLink to="/register" exact activeClassName="active" isActive={this.checkActive}>
+											SignUp
+										</NavLink>
 									</li>
-								</>
+								</div>
 							) : (
-								<>
+								<div>
 									<li>
-										<Link to="/">Home</Link>
+										<NavLink to="/" exact activeClassName="active" isActive={this.checkActive}>
+											Home
+										</NavLink>
 									</li>
 									<li>
 										<a href="/" onClick={this.handleLogout}>
 											Logout
 										</a>
 									</li>
-								</>
+								</div>
 							)}
 						</ul>
 					</div>
@@ -52,96 +60,9 @@ class Navbar extends Component {
 }
 
 function mapStateToProps(state) {
-	// console.log(state, "nav map state");
 	return {
 		user: state.currentUser
 	};
 }
-
-// import { Link, NavLink } from "react-router-dom";
-// import { connect } from "react-redux";
-
-// class Navbar extends Component {
-// 	constructor(props) {
-// 		super(props);
-// 		this.state = {
-// 			activeNav: "home"
-// 		};
-// 	}
-
-// 	changeActive = activeNav => {
-// 		this.setState({ activeNav });
-// 	};
-// 	logout = () => {
-// 		localStorage.setItem("jwt", "");
-// 		this.props.dispatch({ type: "LOG_OUT", data: null });
-// 		this.setState({ activeNav: "home" });
-// 	};
-// 	render() {
-// 		const { activeNav } = this.state;
-
-// 		return (
-// 			<div className="navbar-box">
-// 				<nav className="">
-// 					<div className="nav-wrapper container">
-// 						<Link onClick={this.changeActive} to="/" className="brand-logo, logo">
-// 							Social Media Tracker
-// 						</Link>
-// 						<ul id="nav-mobile" className="right hide-on-med-and-down">
-// 							<li className={activeNav === "home" ? "active" : ""}>
-// 								<NavLink onClick={() => this.changeActive("home")} to="/">
-// 									Home
-// 								</NavLink>
-// 							</li>
-// 							{!this.props.currentUser ? (
-// 								<>
-// 									<li className={activeNav === "login" ? "active" : ""}>
-// 										<NavLink
-// 											onClick={() => this.changeActive("login")}
-// 											to={{
-// 												pathname: "/login",
-// 												navObj: {
-// 													activeNav: this.changeActive
-// 												}
-// 											}}
-// 										>
-// 											Login
-// 										</NavLink>
-// 									</li>
-// 									<li className={activeNav === "register" ? "active" : ""}>
-// 										<NavLink
-// 											onClick={() => this.changeActive("register")}
-// 											to={{
-// 												pathname: "/register",
-// 												navObj: {
-// 													activeNav: this.changeActive
-// 												}
-// 											}}
-// 										>
-// 											SignUp
-// 										</NavLink>
-// 									</li>
-// 								</>
-// 							) : (
-// 								<>
-// 									<li>
-// 										<NavLink name="home" onClick={this.logout} to="/">
-// 											Logout
-// 										</NavLink>
-// 									</li>
-// 								</>
-// 							)}
-// 						</ul>
-// 					</div>
-// 				</nav>
-// 			</div>
-// 		);
-// 	}
-// }
-
-// function mapStateToProps(state) {
-// 	// console.log(state, "login mapStateToProps");
-// 	return { currentUser: state.currentUser.user };
-// }
 
 export default connect(mapStateToProps)(Navbar);
